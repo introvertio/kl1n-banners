7bd87420d47455025b2096a4fa8543d1397070f6
Oyindamola .P. Dosumu <introvertllc2019@gmail.com>
2025-01-16 19:26:08 +0100

    increase button height and adjust banner section height

--- app/components/sections/BannerGenerator.tsx
+++ app/components/sections/BannerGenerator.tsx
@@ -6,7 +6,7 @@ import { cabinetFont } from "../styling/font-classes";
 export default function BannerGenerator() {
   return (
     <section
-      className={`h-svh overflow-y-auto flex flex-col md:flex-row items-center justify-center snap-start w-full relative `}
+      className={`h-[120svh] overflow-y-auto flex flex-col md:flex-row items-center justify-center snap-start w-full relative `}
     >
       <BannerPreview />
       <MainController />
