6dff720e2a79364f4c95bb2f648681a860635900
Oyindamola .P. Dosumu <introvertllc2019@gmail.com>
2025-01-17 12:05:29 +0100

    added seo and pwa to head of root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Kl1n Banners",
   description: "Generate Banners for your social media",
+  openGraph: {
+    title: "Kl1n Banners | Free! Social Banners for everyone",
+    description: "Create custom Banners for your social media for free!",
+    images: [
+      {
+        url: "https://banners.kl1n.link/og.png",
+      },
+    ],
+  },
 };
 
 const poppins = Poppins({
@@ -21,6 +30,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-icon"
+          href="/apple-icon.png"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="icon"
+          href="/icon.png"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={` ${poppins.className} antialiased`}>{children}</body>
     </html>
   );
